a893292759f5fe47b3aa2d696d4cc5bed9560d51
Adarsh <adarshhp14@gmail.com>
2021-11-03 16:39:26 +0530

    First Output message file with Var

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,12 @@
+#output "abc" {
+#  value = "Hello Welcome to the world of Terraform"
+#}
+
+
+variable "abc" {
+  default = "100"
+}
+
 output "abc" {
-  value = "Hello Welcome to the world of Terraform"
+  value = var.abc
 }

244b93ff1d1ae61e5578ff12ed95eeaf5b71160f
Adarsh <adarshhp14@gmail.com>
2021-11-08 11:54:03 +0530

    Example main update

--- Example/main.tf
+++ Example/main.tf
@@ -2,57 +2,19 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "sample" {
-  ami                    = "ami-0dc863062bc04e1de"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.allow_sample.id, "sg-09a37943e80d0bf07"]
-
-  tags = {
-    Name = "sample"
-  }
-}
-
-resource "aws_security_group" "allow_sample" {
-  name        = "allow_sample"
-  description = "Allow sample traffic"
-
-  ingress = [
-    {
-      description      = "TLS from VPC"
-      from_port        = 22
-      to_port          = 22
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      security_groups  = []
-      self             = false
-    }
-  ]
-
-  egress = [
-    {
-      description      = "egress"
-      from_port        = 0
-      to_port          = 0
-      protocol         = "-1"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
-      prefix_list_ids  = []
-      security_groups  = []
-      self             = false
-    }
-  ]
-
-  tags = {
-    Name = "allow_sample"
-  }
-}
-
 terraform {
   backend "s3" {
     bucket = "terraform-puni"
     key    = "example/sample/terraform.tfstate"
     region = "us-east-1"
   }
+}
+
+module "ec2" {
+  source = "./ec2"
+  SGID = module.sg.SGID
+}
+
+module "sg" {
+  source = "./sg"
 }
